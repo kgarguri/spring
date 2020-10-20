03216c9c6479c8ea53965efe5b6806cceb812b60
kgaeguri <robert.kim819@gmail.com>
2020-10-20 17:52:00 +0900

    .

---
+++ javascript_jquery/WebContent/jquery-my-0.1.js
@@ -0,0 +1,33 @@
+window.jQuery=function(selector){
+	if( typeof selector=='string'){
+		
+	}else if(typeof selector=='function'){
+		window.addEventListener('load',selector);		
+	}else if(typeof selector=='object'){
+		//DOM Object
+		//DOM Object --> jQueryWrapperObject 변경
+	}
+	var elementNodeList = document.querySelectorAll(selector);
+	var elementNode = document.querySelector(selector);
+	var jQueryWrapperObject={
+		'elementNodeList':elementNodeList,
+		'elementNode':elementNode,
+		'css':function(colorArg){
+			for(var i=0;i<elementNodeList.length;i++){
+				this.elementNodeList.item(i).style.color=colorArg;
+			}
+		},
+		'text':function(textArg){
+			if(textArg){
+			   for(var i=0;i<elementNodeList.length;i++){
+			       elementNodeList.item(i).firstChild.nodeValue=textArg;
+			   }		
+			
+			}else{
+				return this.elementNode.firstChild.nodeValue;
+			}
+		}
+	};
+	return jQueryWrapperObject;
+};
+window.$=window.jQuery;
